334449b076767e54a231553742a09063d09aabdc
Kristine <kteich88@gmail.com>
2016-09-20 15:17:16 -0400

    Created method for player turns, play again and try again for invalid input.

--- game_of_sticks.rb
+++ game_of_sticks.rb
@@ -7,13 +7,36 @@ def take_sticks(player, total_sticks)
   elsif total_sticks.sticks == 1 && player == 1
     total_sticks.sticks = total_sticks.sticks - player
   else
-    try_again()
+    print "There is #{total_sticks.sticks} stick on the board. You must pick up #{total_sticks.sticks} stick. "
+    sticks = gets.chomp.to_i
+      total_sticks = take_sticks(sticks, total_sticks)
   end
   return total_sticks
 end
 
+def whos_turn(player, other, total_sticks)
+  @current_player = player
+  @opponent = other
+  puts "There are #{total_sticks.sticks} sticks on the board."
+  print "#{@current_player}: How many sticks would you like to take? You may choose a number between 1 and 3: "
+  sticks = gets.chomp.to_i
+    total_sticks = take_sticks(sticks, total_sticks)
+end
+
+
 def try_again()
   puts "Invalid input. Would you like to try again?"
+  print "Type \'retry\' to try again or \'exit\' to return to the Terminal: "
+  input = gets.chomp.downcase
+  if input == "retry"
+    main
+  else
+    exit
+  end
+end
+
+def play_again()
+  puts "Would you like to play again?"
   print "Type \'retry\' to try again or \'exit\' to return to the Terminal: "
   input = gets.chomp.downcase
   if input == "retry"
@@ -34,19 +57,13 @@ def main()
    end
 
   until total_sticks.sticks == 0
-    puts "There are #{total_sticks.sticks} sticks on the board."
-    print "Player 1: How many sticks would you like to take? You may choose a number between 1 and 3: "
-    p1_take_sticks = gets.chomp.to_i
-      total_sticks = take_sticks(p1_take_sticks, total_sticks)
-    puts "There are #{total_sticks.sticks} sticks on the board."
-    print "Player 2: How many sticks would you like to take? You may choose a number between 1 and 3: "
-    p2_take_sticks = gets.chomp.to_i
-      total_sticks = take_sticks(p2_take_sticks, total_sticks)
+    total_sticks = whos_turn('Player 1', 'Player 2', total_sticks)
+    total_sticks = whos_turn('Player 2', 'Player 1', total_sticks)
   end
   if total_sticks.sticks == 0
     puts "There are #{total_sticks.sticks} sticks on the board."
-    # how to let player know they won!
-    try_again()
+    puts "#{@opponent} lost... #{@current_player} WINS!"
+    play_again()
   end
 end
 
