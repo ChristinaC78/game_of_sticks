f90fb774fa233126daadb961c8424e956ddf6ec2
Kristine <kteich88@gmail.com>
2016-09-20 21:27:31 -0400

    Fixed troubleshooting, total stick value does not go beyond zero. Allowed replay without recalling main method.

--- game_of_sticks.rb
+++ game_of_sticks.rb
@@ -1,17 +1,44 @@
 require 'pry'
 
-def take_sticks(player, total_sticks)
-  if total_sticks != 1
-    (1..3).include?(player)
-    total_sticks = total_sticks - player
-  elsif total_sticks == 1 && player == 1
+def get_game_mode_option
+  puts "Lets play a game of Pick Up Sticks!"
+  puts "Options for Gameplay:"
+  puts "Play against a friend type \'1\'"
+  puts "Play against the computer type \'2\'"
+  option = nil
+
+  until option == "1" || option == "2" do
+    print "Which option do you choose? "
+    option = gets.chomp
+  end
+
+  option
+end
+
+def get_initial_sticks
+  initial_sticks = 0
+
+  until (10..100).include?(initial_sticks) do
+    print "How many sticks are on the table? Please choose a number between 10 and 100: "
+    initial_sticks = gets.chomp.to_i
+  end
+
+  initial_sticks
+end
+
+def pick_up_sticks(player, total_sticks)
+  if total_sticks >= 3
     total_sticks = total_sticks - player
   else
-    print "There is/are #{total_sticks} stick(s) on the board. You must pick up #{total_sticks} stick(s). "
-    sticks = gets.chomp.to_i
-    total_sticks = take_sticks(sticks, total_sticks)
+    if total_sticks == 2 && (1..2).include?(player)
+      total_sticks = total_sticks - player
+    else
+      total_sticks == 1 && player == 1
+      total_sticks = total_sticks - player
+    end
   end
-  return total_sticks
+
+  total_sticks
 end
 
 def whos_turn(player, other, total_sticks)
@@ -27,69 +54,43 @@ def whos_turn(player, other, total_sticks)
     end
   else
     sticks = rand(1..3)
-    puts "AI takes #{sticks}"
+    puts "AI chose #{sticks}"
   end
-  total_sticks = take_sticks(sticks, total_sticks)
+
+  total_sticks = pick_up_sticks(sticks, total_sticks)
 end
 
 def game_over(total_sticks)
-  if total_sticks == 0
-    puts "There are #{total_sticks} sticks on the board."
+  if total_sticks <= 0
+    puts "There are no sticks on the board."
     puts "#{@opponent} lost... #{@current_player} WINS!"
     play_again()
   end
 end
-
-# def try_again()
-#   puts "Invalid input. Would you like to try again?"
-#   print "Type \'retry\' to try again or \'exit\' to return to the Terminal: "
-#   input = gets.chomp.downcase
-#   if input == "retry"
-#     main
-#   else
-#     exit
-#   end
-# end
 
 def play_again()
   puts "Would you like to play again?"
   print "Type \'again\' to play again or \'exit\' to return to the Terminal: "
   input = gets.chomp.downcase
   if input == "again"
-    main
+    option = get_game_mode_option
+
+    total_sticks = get_initial_sticks
+
+    until total_sticks == 0
+      player2 = option == "1" ? 'Player 2' : 'AI'
+
+      total_sticks = whos_turn('Player 1', player2, total_sticks)
+      game_over(total_sticks)
+      total_sticks = whos_turn(player2, 'Player 1', total_sticks)
+      game_over(total_sticks)
+    end
   else
     exit
   end
 end
 
-def get_game_mode_option
-  option = nil
-
-  until option == "1" || option == "2" do
-    print "Which option do you choose? "
-    option = gets.chomp
-  end
-
-  option
-end
-
-def get_initial_sticks
-  initial_sticks = 0
-
-  until (10..100).include?(initial_sticks) do
-    print "How many sticks are on the table initially? Please choose a number between 10 and 100: "
-    initial_sticks = gets.chomp.to_i
-  end
-
-  initial_sticks
-end
-
-def main()
-  puts "Welcome to the Game of Sticks!"
-  puts "Options for Gameplay:"
-  puts "Play against a friend (1)"
-  puts "Play against the computer (2)"
-
+def main
   option = get_game_mode_option
 
   total_sticks = get_initial_sticks
